93be532b4e8bec8f4fbc90ca179ab66cd29e41c1
Joshu2212 <vsjoprassana.2006@gmail.com>
2026-07-08 18:45:52 +0530

    Sum of 2 nums without user input

---
+++ Sum_of_two_numbers.java
@@ -0,0 +1,9 @@
+// without user input
+public class Sum_of_two_numbers {
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 5;
+        int res = num1+num2;
+        System.out.printf("The sum of %d and %d is %d",num1,num2,res);
+    }
+}
